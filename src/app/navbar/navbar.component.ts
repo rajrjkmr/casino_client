2484aed12decfc3dcae71e0cddd987cbc5db2120
sureshairlift <rajsuresh@airliftusa.com>
2021-02-08 20:42:36 +0530

    result view

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,23 +16,51 @@ export class NavbarComponent implements OnInit {
   dialogRef: any;
   userInfo: any;
   directConList = [];
-  time=''
+  time = ''
   constructor(
     private route: Router, private activeRoute: ActivatedRoute) { }
 
-
+  selectedTime = '11:00 am'
   ngOnInit() {
-
     setInterval((() => this.setTime()), 1000);
   }
 
 
   setTime() {
-var startTime = moment("09:16:59 pm", "HH:mm:ss a");
-var endTime = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
-var duration = moment.duration(startTime.diff(endTime));
-      this.time =[duration.get('hours'),duration.get('minutes'), duration.get('seconds')].join(':')
-
+    var startTime = moment(this.selectedTime, "HH:mm:ss a");
+    var endTime = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
+    var duration = moment.duration(startTime.diff(endTime));
+    let l = duration.get('hours') + duration.get('minutes') + duration.get('seconds');
+    if (l <= 0) {
+      this.time = '00:00:00'
+      // this.getNext();
+      // var startTime = moment(this.selectedTime, "HH:mm:ss a");
+      // var endTime = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
+      // var duration = moment.duration(startTime.diff(endTime));
+      // let l = duration.get('hours') + duration.get('minutes') + duration.get('seconds');
+      // this.time = [duration.get('hours'), duration.get('minutes'), duration.get('seconds')].join(':')
+    } else {
+      this.time = [duration.get('hours'), duration.get('minutes'), duration.get('seconds')].join(':')
+    }
   }
+
+  getNext() {
+    let t = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
+    if (t < moment('8:00 pm', "HH:mm:ss a")) {
+      this.selectedTime = '11:00 am'
+    } else if (t < moment('1:00 pm', "HH:mm:ss a")) {
+      this.selectedTime = '1:00 pm'
+    } else if (t < moment('6:00 pm', "HH:mm:ss a")) {
+      this.selectedTime = '6:00 pm'
+    } else if (t < moment('8:00 pm', "HH:mm:ss a")) {
+      this.selectedTime = '8:00 pm'
+    }else{
+      this.selectedTime= moment().add(1).format('MM-DD-YYYY hh:mm:ss a')
+      // console.log("ss")
+      // this.selectedTime = moment().add(1).format('MM-DD-YYYY hh:mm:ss a')
+      // console.log(this.selectedTime )
+    }
+  }
+
 
 }

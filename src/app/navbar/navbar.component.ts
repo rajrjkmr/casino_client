ab96c8f86c2dabb66a72918cb8e7fc4814e6202d
sureshairlift <rajsuresh@airliftusa.com>
2021-02-09 11:51:07 +0530

    local

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -33,12 +33,12 @@ export class NavbarComponent implements OnInit {
     let l = duration.get('hours') + duration.get('minutes') + duration.get('seconds');
     if (l <= 0) {
       this.time = '00:00:00'
-      // this.getNext();
-      // var startTime = moment(this.selectedTime, "HH:mm:ss a");
-      // var endTime = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
-      // var duration = moment.duration(startTime.diff(endTime));
-      // let l = duration.get('hours') + duration.get('minutes') + duration.get('seconds');
-      // this.time = [duration.get('hours'), duration.get('minutes'), duration.get('seconds')].join(':')
+      this.getNext();
+      var startTime = moment(this.selectedTime, "HH:mm:ss a");
+      var endTime = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
+      var duration = moment.duration(startTime.diff(endTime));
+      let l = duration.get('hours') + duration.get('minutes') + duration.get('seconds');
+      this.time = [duration.get('hours'), duration.get('minutes'), duration.get('seconds')].join(':')
     } else {
       this.time = [duration.get('hours'), duration.get('minutes'), duration.get('seconds')].join(':')
     }
@@ -46,13 +46,13 @@ export class NavbarComponent implements OnInit {
 
   getNext() {
     let t = moment(moment().format('hh:mm:ss a'), "HH:mm:ss a");
-    if (t < moment('8:00 pm', "HH:mm:ss a")) {
+    if (t.isAfter(moment('11:00 pm', "HH:mm:ss a"))) {
       this.selectedTime = '11:00 am'
-    } else if (t < moment('1:00 pm', "HH:mm:ss a")) {
+    } else if (t.isAfter(moment('1:00 pm', "HH:mm:ss a"))) {
       this.selectedTime = '1:00 pm'
-    } else if (t < moment('6:00 pm', "HH:mm:ss a")) {
+    } else if (t.isAfter(moment('6:00 pm', "HH:mm:ss a"))) {
       this.selectedTime = '6:00 pm'
-    } else if (t < moment('8:00 pm', "HH:mm:ss a")) {
+    } else if (t.isAfter(moment('8:00 pm', "HH:mm:ss a"))) {
       this.selectedTime = '8:00 pm'
     }else{
       this.selectedTime= moment().add(1).format('MM-DD-YYYY hh:mm:ss a')
